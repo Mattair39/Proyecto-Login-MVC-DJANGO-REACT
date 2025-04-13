30c6936c91959bbc1650aeb30a40faf01802de00
Mattair39 <105869714+Mattair39@users.noreply.github.com>
2025-04-13 16:29:45 -0500

    Desarrollo Refresh Token Cookies

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,11 +1,8 @@
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
 from django.urls import path
-from .views import get_notes, CustomTokenObtainPairView
+from .views import get_notes, CustomTokenObtainPairView, CustomRefreshTokenView
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', CustomRefreshTokenView.as_view(), name='token_refresh'),
     path('notes/', get_notes)
 ]
